04cc87c9e7fc9bc8dafb81ed407299c0faf9781d
Austin Gunsauley <ajgunsauley@gmail.com>
2019-05-11 16:40:05 -0700

    positive trading between player 1 and 2

---
+++ Assets/Scripts/TradeController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeController : MonoBehaviour
+{
+    public GameObject sourceResourceController;
+    public GameObject targetResourceController;
+    private ResourceController sourceResourceControllerScript;
+    private ResourceController targetResourceControllerScript;
+    public int sourceFood;
+    public int sourceEnergy;
+    public int sourcePops;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sourceResourceControllerScript = sourceResourceController.GetComponent<ResourceController>();
+        targetResourceControllerScript = targetResourceController.GetComponent<ResourceController>();
+    }
+
+    public void TradeFoodFromSource(int foodToSend)
+    {
+        sourceResourceControllerScript.TradeFoodAway(1);
+        sourceFood = sourceResourceController.GetComponent<ResourceController>().foodToTrade;
+    }
+
+    public void TradeEnergyFromSource(int energyToSend)
+    {
+        sourceResourceControllerScript.TradeEnergyAway(1);
+        sourceEnergy = sourceResourceController.GetComponent<ResourceController>().energyToTrade;
+    }
+
+    public void TradePopsFromSource(int popsToSend)
+    {
+        sourceResourceControllerScript.TradePopsAway(1);
+        sourcePops = sourceResourceController.GetComponent<ResourceController>().popsToTrade;
+    }
+
+    public void ProcessTrades()
+    {
+        targetResourceControllerScript.GetFoodFromTrade(sourceFood);
+        targetResourceControllerScript.GetEnergyFromTrade(sourceEnergy);
+        targetResourceControllerScript.GetPopsFromTrade(sourcePops);
+        sourceFood = 0;
+        sourceEnergy = 0;
+        sourcePops = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
