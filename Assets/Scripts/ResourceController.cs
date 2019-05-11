70d5aa23bc0e53247b8a0441b75e2aa1787d9d9e
Austin Gunsauley <ajgunsauley@gmail.com>
2019-05-11 12:07:25 -0700

    basic turn system and resource calculation for one player including angry pops

---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public int currentFood;
+    public int currentEnergy;
+    public int currentPops;
+    public int currentPopsAngry;
+
+    public Text currentFoodDisplay;
+    public Text currentEnergyDisplay;
+    public Text currentPopsDisplay;
+    public Text currentPopsAngryDisplay;
+
+    private int popFoodDifference;
+    private int popEnergyDifference;
+    private int popsLostToAnger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UpdateResourceCount();
+    }
+
+    public void UpdateResourceCount()
+    {
+        //check food and energy deficits
+        if (currentFood <= currentPops)
+        {
+            popFoodDifference = (currentPops - currentFood);
+            //popsLostToAnger = popResourceDifference;
+
+        }
+
+        if (currentEnergy <= currentPops)
+        {
+            popEnergyDifference = (currentPops - currentEnergy);
+
+        }
+
+        //calculate final total pop loss
+        popsLostToAnger = Math.Max(popFoodDifference,popEnergyDifference);
+        currentPops -= popsLostToAnger;
+        currentPopsAngry += popsLostToAnger;
+
+        //update mockup UI
+        currentFoodDisplay.text = "Current Food: " + currentFood;
+        currentEnergyDisplay.text = "Current Energy: " + currentEnergy;
+        currentPopsDisplay.text = "Current Pops: " + currentPops;
+        currentPopsAngryDisplay.text = "Current Angry Pops: " + currentPopsAngry;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TurnController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnController : MonoBehaviour
+{
+    public GameObject resourceController;
+    
+    public int currentTurn;
+    public int turnLimit;
+    public Text turnDisplay;
+
+    private bool gameOver;
+    private ResourceController resourceControllerScript;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        resourceControllerScript = resourceController.GetComponent<ResourceController>();
+        Clicked();
+        gameOver = false;
+    }
+
+    public void Clicked()
+    {
+        if ((currentTurn != turnLimit) && (gameOver == false))
+        {
+            currentTurn += 1;
+            turnDisplay.text = "Current Turn: " + currentTurn;
+            resourceControllerScript.UpdateResourceCount();
+        }
+
+        else if (currentTurn == turnLimit)
+        {
+            gameOver = true;
+            turnDisplay.text = "Turn limit exceeded";
+        }
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
