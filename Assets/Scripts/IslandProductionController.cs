088637c76057796906805b1e275d03af95988231
Thierry Treyer <ttreyer@fb.com>
2019-05-11 15:35:23 -0700

    Add ability to updgrade/replace resources

--- Assets/Scripts/IslandProductionController.cs
+++ Assets/Scripts/IslandProductionController.cs
@@ -1,25 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class IslandProductionController : MonoBehaviour {
-    public int food, energy, polution, population;
-
-    public void UpdateProduction() {
-        // Reset counters
-        food = energy = polution = population = 0;
-
-        // Collect resources
-        foreach (Resource r in GetComponentsInChildren<Resource>(false)) {
-            food += r.food;
-            energy += r.energy;
-            polution += r.polution;
-            population += r.population;
-        }
-    }
+    public ResourceValues production;
 
     // Update is called once per frame
     void Update() {
         UpdateProduction();
+    }
+
+    public void UpdateProduction() {
+        production.Reset();
+
+        // Collect resources
+        foreach (Resource r in GetResources()) {
+            production.Add(r.baseValues);
+            production.Add(r.bonusValues);
+        }
+    }
+
+    public void UpgradeTypeWithValues(ResourceType type, ResourceValues addValues) {
+        foreach (Resource r in GetResourcesWithType(type))
+            r.UpgradeValues(addValues);
+    }
+
+    public void ReplaceTypeWithResource(ResourceType type, Resource newResource, bool keepBonus = false) {
+        foreach (Resource r in GetResourcesWithType(type)) {
+            Resource nr = Instantiate(newResource, r.transform.position, r.transform.rotation, r.transform.parent);
+            if (keepBonus)
+                nr.UpgradeValues(r.bonusValues);
+            Destroy(r.gameObject);
+        }
+    }
+
+    private Resource[] GetResources() {
+        return GetComponentsInChildren<Resource>(false);
+    }
+
+    private Resource[] GetResourcesWithType(ResourceType type) {
+        return GetResources().Where(r => r.type == type).ToArray();
     }
 }
--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public enum ResourceType {
     Unknown,
-    ___FOOD__, Crops, Hydroponic,
+    ___FOOD__, Crops, SuperCrops, Hydroponic,
     ___ENERGY__, CoalPower, Windmill,
     ___POPULATION__, Town, City
 }
 
+[System.Serializable]
+public struct ResourceValues {
+    public int food, energy, polution, population;
+
+    public void Reset() {
+        food = energy = polution = population = 0;
+    }
+
+    public ResourceValues Add(ResourceValues v) {
+        food += v.food;
+        energy += v.energy;
+        polution += v.polution;
+        population += v.population;
+
+        return this;
+    }
+}
+
 public class Resource : MonoBehaviour {
     public ResourceType type;
-    public int food, energy, polution, population;
+    public ResourceValues baseValues;
+    public ResourceValues bonusValues;
+
+    public void UpgradeValues(ResourceValues addedValues) {
+        bonusValues.Add(addedValues);
+    }
+
+    public ResourceValues GetValues() {
+        return new ResourceValues()
+            .Add(baseValues)
+            .Add(bonusValues);
+    }
 }
