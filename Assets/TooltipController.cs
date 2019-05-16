0905c139e559136f5be2acb67fa356ad2667b1c9
Thierry Treyer <ttreyer@fb.com>
2019-05-15 23:02:47 -0700

    Make tooltip compatible with UI

--- Assets/TooltipController.cs
+++ Assets/TooltipController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class TooltipController : MonoBehaviour {
+public class TooltipController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public enum TooltipContext {
+        NONE, NOT_UI, UI
+    };
+
+    public TooltipContext context; // Remember what trigger the tooltip
     private GameObject tooltip;
 
     private void Start() {
@@ -12,16 +17,36 @@ public class TooltipController : MonoBehaviour {
         SetTooltipVisibility(false);
     }
 
-    public void OnMouseEnter() {
-        SetTooltipVisibility(true);
-    }
-
-    public void OnMouseExit() {
-        SetTooltipVisibility(false);
-    }
-
     private void SetTooltipVisibility(bool visibility) {
         if (tooltip)
             tooltip.SetActive(visibility);
+    }
+
+    public void OnMouseEnter() {
+        if (context == TooltipContext.NONE) {
+            context = TooltipContext.NOT_UI;
+            SetTooltipVisibility(true);
+        }
+    }
+
+    public void OnMouseExit() {
+        if (context == TooltipContext.NOT_UI) {
+            SetTooltipVisibility(false);
+            context = TooltipContext.NONE;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        if (context == TooltipContext.NONE) {
+            context = TooltipContext.UI;
+            SetTooltipVisibility(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        if (context == TooltipContext.UI) {
+            SetTooltipVisibility(false);
+            context = TooltipContext.NONE;
+        }
     }
 }
